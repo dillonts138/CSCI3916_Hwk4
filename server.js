f3702e6f5f13c5a74660d995b4f4594048e6d381
dillonts138 <78238417+dillonts138@users.noreply.github.com>
2021-04-10 14:35:42 -0600

    Update server.js

--- server.js
+++ server.js
@@ -23,6 +23,20 @@ app.use(passport.initialize());
 
 var router = express.Router();
 
+var allowCrossDomain = function(req, res, next) {
+    if ('OPTIONS' == req.method) {
+      res.header('Access-Control-Allow-Origin', '*');
+      res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
+      res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
+
+app.use(allowCrossDomain);
+
 function getJSONObjectForMovieRequirement(req) {
     var json = {
         headers: "No headers",
