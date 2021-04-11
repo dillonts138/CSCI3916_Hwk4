ac8d34645d9a90a6208a13ffa8f64965f7646e3b
dillonts138 <78238417+dillonts138@users.noreply.github.com>
2021-04-11 03:44:54 -0600

    Update server.js

--- server.js
+++ server.js
@@ -28,10 +28,6 @@ var allowCrossDomain = function(req, res, next) {
      res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
      res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
      res.send(200);
-    }
-    else {
-      next();
-    }
 };
 
 app.use(allowCrossDomain);
